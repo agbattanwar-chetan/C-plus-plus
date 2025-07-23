3b4735583989b3e712fd38c400c2a39c284ff262
chetan <chetanagbattanwar26@gmail.com>
2025-07-23 10:57:06 +0530

    data-types

---
+++ 02.data-types.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // data types
+
+    // 1.0 primitive / built-in data type
+    // 1.1 int
+    // int a = 10;
+    // cout << a<<endl;
+    // cout << sizeof(a) << " bytes" <<endl;
+
+    // int c = 10.5;
+    // cout << c<<endl; // 10
+    // cout << sizeof(c) << " bytes" <<endl;
+
+    // 1.2 float
+    // float b = 10.5;
+    // cout << b<<endl;
+    // cout << sizeof(b) << " bytes" <<endl;
+
+    // float d = 10;
+    // cout << d<<endl; //10
+    // cout << sizeof(d) << " bytes" <<endl;
+
+    // 1.3 double
+    // double b = 10.1234;
+    // cout << b<<endl;
+    // cout << sizeof(b) << " bytes" <<endl;
+
+    // 1.4 char
+    // char f = 'a';
+    // cout << f<<endl;
+    // cout << sizeof(f) << " bytes" <<endl;
+
+    // 1.5 bool
+    // true => 1
+    // false => 0
+    // bool g = true;
+    // cout << g<<endl;
+    // cout << sizeof(g) << " bytes" <<endl;
+
+    // 1.6 void
+
+//     2.0 Derived Data Types
+// Definition: Built from primitive data types to create more complex structures.
+//  2.1 Array
+//  2.2 Pointer
+//  2.3 Function
+//  2.4 Reference 
+
+// 3.0 User-defined Data Types
+//  3.1 struct (Structure)
+//  3.2 union
+//  3.3 enum (Enumeration)
+//  3.4 class 
+
+    }
